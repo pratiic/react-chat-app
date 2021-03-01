ebdb908ca65340f47ff989049c8b73b83485e1d0
pratik bhandari <pratikbhandari9292@gmail.com>
2021-03-01 13:55:13 +0545

    limited the number of messages to 25

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, usePrevious } from "react";
 import { connect } from "react-redux";
 
 import "./chat.scss";
@@ -10,44 +10,83 @@ import Message from "../message/message";
 
 const Chat = ({ currentUser }) => {
 	const [messages, setMessages] = useState([]);
+	const [totalFetchedMessages, setTotalFetchedMessages] = useState(0);
+	const [fetching, setFetching] = useState(false);
 
-	const scrollDiv = useRef(null);
+	const scrollDivBottom = useRef(null);
+	const scrollDivTop = useRef(null);
 
 	useEffect(() => {
-		firestore.collection("messages").onSnapshot(async (snapshot) => {
-			const messagesRef = firestore.collection("messages");
-			const someKindOfThing = await messagesRef
-				.orderBy("createdAt")
-				.get();
-			const messagesFromFirestore = someKindOfThing.docs;
-			setMessages(messagesFromFirestore);
-		});
+		fetchMessages(25);
+		setTotalFetchedMessages(25);
 		//eslint-disable-next-line
 	}, []);
 
 	useEffect(() => {
-		scrollDiv.current.scrollIntoView();
+		scrollDivBottom.current.scrollIntoView();
 	}, [messages]);
+
+	// useEffect(() => {
+	// 	scrollDivTop.current.scrollIntoView();
+	// 	console.log(scrollDivTop.current);
+	// }, [totalFetchedMessages]);
+
+	const fetchMessages = (fetchNumber) => {
+		firestore.collection("messages").onSnapshot(async (snapshot) => {
+			setFetching(true);
+			const messagesRef = firestore.collection("messages");
+			const someKindOfThing = await messagesRef
+				.orderBy("createdAt", "desc")
+				.limit(fetchNumber)
+				.get();
+			const messagesFromFirestore = someKindOfThing.docs;
+			setMessages(messagesFromFirestore);
+			setFetching(false);
+		});
+	};
+
+	const renderMessages = () => {
+		const revMessages = reverseArray(messages);
+
+		return revMessages.map((message) => {
+			const data = message.data();
+			return (
+				<Message
+					text={data.text}
+					key={data.mid}
+					self={data.createdBy === currentUser.uid ? true : false}
+					senderId={data.createdBy}
+				/>
+			);
+		});
+	};
+
+	const reverseArray = (array) => {
+		let reversedArray = [];
+
+		for (let i = array.length - 1; i >= 0; i--) {
+			reversedArray = [...reversedArray, array[i]];
+		}
+
+		return reversedArray;
+	};
+
+	const handleLoadMoreClick = () => {
+		fetchMessages(totalFetchedMessages + 25);
+		setTotalFetchedMessages(totalFetchedMessages + 25);
+	};
 
 	return (
 		<div className="chat">
 			<div className="chat-main">
-				{messages.map((message) => {
-					const data = message.data();
-					return (
-						<Message
-							text={data.text}
-							key={data.mid}
-							self={
-								data.createdBy === currentUser.uid
-									? true
-									: false
-							}
-							senderId={data.createdBy}
-						/>
-					);
-				})}
-				<div className="inner-chat-main" ref={scrollDiv}></div>
+				{messages.length === 25 ? (
+					<p className="load-more" onClick={handleLoadMoreClick}>
+						{fetching ? "loading..." : "load more..."}
+					</p>
+				) : null}
+				<div className="scroll" ref={scrollDivTop}></div>
+				{renderMessages()}
+				<div className="scroll" ref={scrollDivBottom}></div>
 			</div>
 			<MessageField />
 		</div>
